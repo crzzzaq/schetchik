aa820221f52402f5fc96a804ade26ae670bf530e
crzzzaq <98319329+crzzzaq@users.noreply.github.com>
2023-05-15 18:07:37 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,28 +1,49 @@
 let btn = document.querySelector('.addition__btn');
 let input1 = document.querySelector('.addition__name1');
-let name = document.querySelector('.purcashes__name');
-let price = document.querySelector('.purcashes__price');
+let name = document.querySelector('tbody .purcashes__name');
+let price = document.querySelector('tbody .purcashes__price');
 let input2 = document.querySelector('.addition__name2');
-let category = document.querySelector('.purcashes__category');
+let category = document.querySelector('tbody .purcashes__category');
 let select = document.querySelector('.select');
 
+let tbody = document.querySelector('tbody');
+let close = document.querySelector('close');
 
 
 btn.addEventListener('click', () => {
-    let element = document.createElement('li');
+    tbody.insertAdjacentHTML('beforeEnd',
+
+        `<tr class="purcashes__row">
+            <td class="purcashes__name">${input1.value}</td>
+            <td class="purcashes__category">${input2.value}</td>
+            <td class="purcashes__price">${select.value}</td>
+            <td class="purcashes__del">
+                <img src="img/close.png" class="close">
+            </td>
+    </tr>` )
+})
+
+tbody.addEventListener('click', (evt) => {
+    let target = 
+})
+
+
+
+/* 
+let element = document.createElement('td');
     element.textContent = input1.value;
     name.append(element);
 })
 
 btn.addEventListener('click', () => {
-    let element = document.createElement('li');
-    element.className = 'price'
+    let element = document.createElement('td');
     element.textContent = input2.value;
     price.append(element);
 })
 
 btn.addEventListener('click', () => {
-    let element = document.createElement('li');
+    let element = document.createElement('td');
     element.textContent = select.value;
     category.append(element);
 })
+ */
